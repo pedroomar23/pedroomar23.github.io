9fc5e3656780f63c0e3ad68f434f9ecd59fcc850
Pedro Omar <pocl9812@gmail.com>
2025-03-25 12:17:50 -0400

    Design

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-const myPresentation = document.createElement('p');
-const wifiConnect = document.createElement('p');
+import Image from 'index.html'
 
-myPresentation.body.textContent = "Hi, I'm Pedro Omar iOS Developer, I'm 25 years old.";
+
+const myPresentation = document.createElement("p");
+const wifiConnect = document.createElement("p");
+
+myPresentation.body.textContent =
+	"Hi, I'm Pedro Omar iOS Developer, I'm 25 years old.";
 document.body.appendChild(myPresentation);
 
-wifiConnect.body.textContent = "Wifi Connect is a app that help to the users to use the Etecsa services with security nad stability.";
+wifiConnect.body.textContent =
+	"Wifi Connect is a app that help to the users to use the Etecsa services with security nad stability.";
 document.body.appendChild(wifiConnect);
 
-const wifi = document.querySelector('img');
+const wifi = document.querySelector("img");
 
 wifi.onclick = () => {
-    const src = wifi.getAttribute('src');
-
-    
-}
-
+	const src = wifi.getAttribute("src");
+};
