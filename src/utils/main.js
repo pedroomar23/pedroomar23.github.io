7cf2fc4bd3a2efcacd45940be639f9e631c70ecb
Pedro Omar <pocl9812@gmail.com>
2025-04-02 05:18:34 -0400

    Changes

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,30 +1,116 @@
-const myPresentation = document.createElement("p");
-const wifiConnect = document.createElement("p");
+const nombre = "Pedro Omar";
+console.log(nombre);
 
-myPresentation.body.textContent =
-	"Hi, I'm Pedro Omar iOS Developer, I'm 25 years old.";
-document.body.appendChild(myPresentation);
+const name = 'Carlos';
+console.log(name);
 
-wifiConnect.body.textContent =
-	"Wifi Connect is a app that help to the users to use the Etecsa services with security nad stability.";
-document.body.appendChild(wifiConnect);
+var document = false;
 
-const wifi = document.querySelector("img");
+var nombre1 = 15;
+var nombre2 = 20;
 
-const profile = document.querySelector('.profile');
-profile.style.justifyContent = 'center';
+if (nombre = nombre1) {
+    console.log(nombre1);
+} else {
+    console.log(nombre2);
+}
 
-document.getElementById('alternarBtn').addEventListener('click', function() {
-	const contenedor = document.getElementById('.container');
-	contenedor.classList.toggle('alternar-vertical');
-	
-	const lateral = document.querySelector('.about');
-	if(contenedor.classList.contains('alternar-vertical')) {
-		lateral.textContent = "Texto abajo";
-	} else {
-		lateral.textContent = "Texto al lado";
-	}
-});
+var x = 20;
+var y = 20;
+
+alert(x);
+
+function suma (x,y) {
+    return x + y;
+}
+console.log(suma(20,20))
+
+if (x = y) {
+    console.log(x);
+} else if (x < y) {
+    console.log(y);
+} else {
+    console.log(x);
+}
+
+function multiplicacion (x,y) 
+{
+    z = x * y;
+    return z;
+}
+
+multiplicacion(x,y);
+
+var nombres = 10;
+
+for (nombres + 1; nombres - 2;) 
+    {
+    console.log(nombres);
+}
+
+var i = 1;
+
+for (i = 2; i +2; i - 2) 
+{
+    console.log(i);
+}
+
+switch (nombres) 
+{
+    case 1:
+        nombres >= 5
+    case 2:
+        nombres = 5
+    default:
+        console.log(nombres);
+}
+
+// Una forma 
+Object (nombres); {
+    // Declarar un objeto 
+}
+
+// Otra forma
+object (LoginModel); {
+    var username = ""
+    var password = ""
+    var idRequest = ""
+    var captchatext = ""
+}
+
+function text() 
+{
+    if (nombre = 5) 
+    {
+        console.log(nombre);
+    } 
+    else if (nombre <= 5) 
+    {
+        console.log(nombre);
+    } 
+    else {
+        console.log(x);
+    }
+}
+
+const login = 'https://www.nauta.cu:5002/login';
+var loginResponse = false;
+
+function login() 
+{
+    if (login) 
+    {
+        console.log(login);
+    } 
+    else 
+    {
+        console.log(login)
+    }
+}
+
+
+
+
 
 
 
