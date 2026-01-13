0775669b01829fc30eb51538e5c1397f1aaf0460
Pedro Omar <pocl9812@gmail.com>
2026-01-13 08:13:04 -0500

    fix: picture

---
+++ src/scripts/wificonnect.js
@@ -0,0 +1,6 @@
+// Wifi Connect Project
+const text = document.getElementById("wifi-connect");
+const textContent =
+  "Wifi Connect is an app that helps users to use Etecsa services with security and stability. Also with this app the users gonna save, edits and share accounts.";
+
+text.textContent = textContent;
